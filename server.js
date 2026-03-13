ea85aef335fcb51fecaa34842d04be696fcb743f
SUBIKSHA-11 <sksubiksha11@gmail.com>
2026-03-13 16:17:40 +0530

    Fix wildcard route crash on Render

--- server.js
+++ server.js
@@ -40,12 +40,8 @@ app.use("/api", require("./routes/departmentRoutes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendDistPath));
 
-  app.get("/*", (req, res, next) => {
-    if (req.path.startsWith("/api") || req.path.startsWith("/uploads")) {
-      return next();
-    }
-
-    return res.sendFile(path.join(frontendDistPath, "index.html"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"));
   });
 }
 
